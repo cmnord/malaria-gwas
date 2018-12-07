b367072cdee58e55a8894acee29b2a7202e53185
kstromhaug <kstrom@mit.edu>
2018-12-07 15:53:13 -0500

    pairwise now goes from running the multiple alignment on multiple files in an outer folder, makes pairwise fastas, and then does the alighment for each one. finally parses the similarities between then and spits out a file

--- data/receptor_genes/pairwise.sh
+++ data/receptor_genes/pairwise.sh
@@ -1,11 +1,18 @@
+######## for running mulltiple alignment on multiple files
+########-------------------------------------------------
+for file in duffy/outer/*
+do muscle -in "$file" -out ${file:0:(-3)}.afa
+done
+
+######## get the pairwise fastas, to put in a pairwise folder
+########-----------------------------------------------------
+mkdir duffy/outer/pairwise1
+python2 make_pairwise_fastas.py "duffy/outer/duffy_all_primates_outer1.fa"
+
 ######## for doing pairwise alighments and putting all the results in the same file
 ########----------------------------------------------------------------------------
+
 for file in duffy/outer/pairwise1/*
 do muscle -in "$file" >> duffy/outer/pairwise1/pairwise1.afa
 done
-
-######## for running mulltiple alignment on multiple files
-########-------------------------------------------------
-#for file in duffy/outer/*
-#do muscle -in "$file" -out ${file:0:(-3)}.afa
-#done
+python2 pairwise_similarities.py "duffy/outer/pairwise1/pairwise1.afa"
